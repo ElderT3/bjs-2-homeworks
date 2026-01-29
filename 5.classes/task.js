9533de0d82d9e093c15d4fbb8ea0add0181ffa41
Aleksandr <Sasha-stx555@mail.ru>
2026-01-29 10:09:13 +0300

    Обновлённый task.js со всеми задачами

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,15 +62,103 @@ class DetectiveBook extends Book {
   }
 }
 
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    return this.books.find(book => book[type] === value) || null;
+  }
+
+  giveBookByName(bookName) {
+    const bookIndex = this.books.findIndex(book => book.name === bookName);
+
+    if (bookIndex === -1) {
+      return null;
+    }
+
+    return this.books.splice(bookIndex, 1)[0];
+  }
+}
+
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {}; // структура: { "физика": [5, 4], "химия": [5] }
+  }
+
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) return; // валидируем оценку
+
+    if (!this.marks[subject]) {
+      this.marks[subject] = [];
+    }
+
+    this.marks[subject].push(mark);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) return 0;
+
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    if (subjects.length === 0) return 0;
+
+    const totalAverage = subjects.reduce(
+      (acc, subject) => acc + this.getAverageBySubject(subject),
+      0
+    );
+
+    return totalAverage / subjects.length;
+  }
+}
+
 const picknick = new FantasticBook(
   "Аркадий и Борис Стругацкие",
   "Пикник на обочине",
   1972,
   168
 );
-
-console.log(picknick.author); // "Аркадий и Борис Стругацкие"
 picknick.state = 10;
-console.log(picknick.state); // 10
 picknick.fix();
-console.log(picknick.state); // 15
+
+const library = new Library("Библиотека имени Ленина");
+library.addBook(
+  new DetectiveBook(
+    "Артур Конан Дойл",
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  )
+);
+library.addBook(picknick);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+const book = library.giveBookByName("Машина времени");
+book.state = 20; // портим книгу
+book.fix(); // чиним
+library.addBook(book);
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); // не добавится
+
+console.log(student.getAverageBySubject("физика")); // 4.5
+console.log(student.getAverageBySubject("биология")); // 0
+console.log(student.getAverage()); // 4.75
